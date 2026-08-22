db80fed4c504bf970b97715a7f7bc998132e4c10
VIP <noirwolvex@gmail.com>
2026-08-22 05:44:00 +0300

    Add rich book metadata migration

---
+++ prisma/migrations/20260822050000_add_book_metadata/migration.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "public"."Book"
+ADD COLUMN "features" TEXT,
+ADD COLUMN "targetAudience" TEXT,
+ADD COLUMN "bookSize" TEXT,
+ADD COLUMN "pageCount" INTEGER,
+ADD COLUMN "seriesParts" TEXT;
