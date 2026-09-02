6c17ea4f8d1c2b205e980d66b8512eb358ede5c2
VIP <noirwolvex@gmail.com>
2026-09-02 17:26:30 +0300

    Add platforms section to SPACE-ZONE home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import FeaturedServices from "@/components/FeaturedServices";
+import FeaturedPlatforms from "@/components/FeaturedPlatforms";
 import FeaturedTools from "@/components/FeaturedTools";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <main className="flex-1 flex flex-col bg-slate-50 dark:bg-[#050505] transition-colors">
         <Hero />
         <FeaturedServices />
+        <FeaturedPlatforms />
         <FeaturedTools />
       </main>
     </>
