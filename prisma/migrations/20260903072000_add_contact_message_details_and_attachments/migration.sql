097af7440007760439839be72e09a324856e42e4
VIP <noirwolvex@gmail.com>
2026-09-03 10:15:19 +0300

    chore: align contact migration timestamp

--- prisma/migrations/20260903072000_add_contact_message_details_and_attachments/migration.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "ContactMessage"
-  ADD COLUMN "contactType" TEXT NOT NULL DEFAULT 'Inquiry',
-  ADD COLUMN "details" TEXT,
-  ADD COLUMN "attachmentUrl" TEXT,
-  ADD COLUMN "attachmentName" TEXT;
