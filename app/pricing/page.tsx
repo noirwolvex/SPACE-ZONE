3278144b5cc9a2f92d49eee15291760ed4c1c64e
VIP <noirwolvex@gmail.com>
2026-09-05 14:37:05 +0300

    Add public pricing page

---
+++ app/pricing/page.tsx
@@ -0,0 +1,7 @@
+import PricingPlans from "@/components/PricingPlans";
+
+export const dynamic = "force-dynamic";
+
+export default function PricingPage(){
+  return <main className="min-h-screen bg-slate-50 pt-24 text-slate-900 dark:bg-[#050505] dark:text-white"><PricingPlans/></main>;
+}
